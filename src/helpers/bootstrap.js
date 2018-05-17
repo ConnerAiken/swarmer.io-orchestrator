e2a5a9386304fc417478807b3b2a880314f632d3
Conner Aiken <conner@fittedtech.com>
2018-05-17 10:35:35 -0700

    Add express, body-parser and add example endpoints

--- src/helpers/bootstrap.js
+++ src/helpers/bootstrap.js
@@ -1,10 +1,21 @@
 import utils from "./utils.js";
 import path from "path";
 import dotenv from "dotenv";
+import DigitalOcean from "do-wrapper";
+import express from "express";
+import bodyParser from "body-parser";
 
-
+global.express = express;
 global.path = path;
 global.dotenv = dotenv;
 global.utils = utils;
  
 utils.loadENV(); 
+ 
+global.api = new DigitalOcean(process.env.doApiKey, process.env.doPageSize);
+
+global.app = express();
+global.app.use(bodyParser.json());
+global.app.use(bodyParser.urlencoded({
+    extended: true
+}))
--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-import './helpers/bootstrap';  
+import './helpers/bootstrap';
+ 
+const digitalOcean = {
+    account: {
+
+    },
+    recentActions: [
+
+    ],
+    droplets: [
+
+    ]
+};
+
+// 0. Get account/droplet info
+api.account().then((data) => {
+    digitalOcean.account = data.body;
+});
+
+// 1. Get recent actions
+api.accountGetActions({includeAll: true}).then((data) => { 
+    digitalOcean.recentActions = data.body.actions;
+});
+
+// 2. get list of all droplets
+api.dropletsGetAll({includeAll: true}).then((data) => {  
+    digitalOcean.droplets = data.body;
+});
+
+
+// Listen for events
+app.post('/register', (req, res) => {
+    if(!req.body.account || req.body.plan) { 
+        res.send("Invalid request.");
+    } 
+});
+
+app.post('/deregister', (req, res) => {
+    if(!req.body.account) { 
+        res.send("Invalid request.");
+    } 
+});
+
+app.post('/queue', (req, res) => {
+    if(!req.body.account || req.body.runners) { 
+        res.send("Invalid request.");
+    }
+    
+    res.send("Queueing test runner");
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
