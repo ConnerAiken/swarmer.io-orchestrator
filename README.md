6c86573815d65c7d708605a30074da865240af85
conneraiken <conner@fittedtech.com>
2018-08-31 09:46:38 -0700

    Add assumptions to README

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Swarmer.io - Orchestrator
 
-This module is responsible for spinning up the agents and managing the agent->digitalocean api chatter.
+This microservice is responsible for spinning up the agents and managing the agent->digitalocean communication.
+
+It assumes that there are tags present on the nodes once spun up. Those tags indicate that they are part of the swarmer.io cluster and have
+a number indicating the free # of test runners or docker container allocated spaces.
+
+- `swarmerio-node` -> Indicates that the droplet is part of the cluster
+- `free-tr-#` =>  # of free test runner instances
+- `free-dc-#` => # of free spaces for docker containers
 
 ## Requirements
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import utils from './helpers/utils';
+import _ from "lodash";
 utils.bootstrap();
  
 const promises = [];
@@ -10,22 +11,19 @@ utils.log("Downloading account data from DigitalOcean..");
 // 0. Get account/droplet info
 promises.push(api.account().then((data) => {
     utils.log(`Received ${Object.keys(data.body).length} fields about the service account.`);
-    digitalOcean.account = data.body; 
-    console.log(digitalOcean.account); 
+    digitalOcean.account = data.body;  
 }));
 
 // 1. Get recent actions
 promises.push(api.accountGetActions({includeAll: true}).then((data) => { 
     utils.log(`Received ${data.body.length} recent events.`);
-    digitalOcean.recentActions = data.body; 
-    console.log(digitalOcean.recentActions);
+    digitalOcean.recentActions = data.body;  
 }));
 
 // 2. get list of all droplets
 promises.push(api.dropletsGetAll({includeAll: true}).then((data) => {
     utils.log(`Received ${data.body.length} droplet data.`);
-    digitalOcean.droplets = data.body;
-    console.log(digitalOcean.droplets);
+    digitalOcean.droplets = data.body.filter(d => _.includes(d.tags, 'swarmerio-node')); 
 })); 
  
 // 3. 
@@ -37,6 +35,8 @@ promises.push(api.dropletsGetAll({includeAll: true}).then((data) => {
 // ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝    ╚═╝   ╚═╝       
                                                                 
 Promise.all(promises).then(() => {
+    console.log(digitalOcean.droplets);
+
     app.listen(process.env.port, () => utils.log(`Listening on port ${process.env.port}!`));
 }).catch((err) => {
     utils.log(`Error! ${err}`)  
