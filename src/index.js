27b411392cd623e98d2740b4c162ae86ed946d73
conneraiken <conner@fittedtech.com>
2018-08-31 09:53:06 -0700

    Set initial loop to 0 for interval

--- src/index.js
+++ src/index.js
@@ -5,39 +5,48 @@ utils.bootstrap();
 const promises = [];
 const { digitalOcean, app, api } = utils;  
  
-utils.log("Service has been deployed and started!"); 
-utils.log("Downloading account data from DigitalOcean..");
+utils.log(":four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: "); 
+utils.log(":100: Service has been deployed and started! :100:"); 
+utils.log(":100: Downloading account data from DigitalOcean.. :100: ");
+utils.log(":100: Service has been deployed and started! :100:"); 
+utils.log(":four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: :four_leaf_clover: "); 
 
-// 0. Get account/droplet info
-promises.push(api.account().then((data) => {
-    utils.log(`Received ${Object.keys(data.body).length} fields about the service account.`);
-    digitalOcean.account = data.body;  
-}));
+let intervalMilis = 0;
+setInterval(() => {
+    // 0. Get account/droplet info 
+    promises.push(api.account().then((data) => {
+        utils.log(`:thinking_face: Received ${Object.keys(data.body).length} fields about the service account.`);
+        digitalOcean.account = data.body;  
+    }));
+    
+    // 1. Get recent actions
+    promises.push(api.accountGetActions({includeAll: true}).then((data) => { 
+        utils.log(`:thinking_face: Received ${data.body.length} recent events.`);
+        digitalOcean.recentActions = data.body;  
+    }));
+    
+    // 2. get list of all droplets
+    promises.push(api.dropletsGetAll({includeAll: true}).then((data) => {
+        utils.log(`:thinking_face: Received ${data.body.length} droplet data.`);
+        digitalOcean.droplets = data.body.filter(d => _.includes(d.tags, 'swarmerio-node')); 
+    })); 
 
-// 1. Get recent actions
-promises.push(api.accountGetActions({includeAll: true}).then((data) => { 
-    utils.log(`Received ${data.body.length} recent events.`);
-    digitalOcean.recentActions = data.body;  
-}));
+    // 3. Fulfill promises and do sumthin
+    Promise.all(promises).then(() => {
+        console.log(digitalOcean.droplets); 
+    });
 
-// 2. get list of all droplets
-promises.push(api.dropletsGetAll({includeAll: true}).then((data) => {
-    utils.log(`Received ${data.body.length} droplet data.`);
-    digitalOcean.droplets = data.body.filter(d => _.includes(d.tags, 'swarmerio-node')); 
-})); 
- 
-// 3. 
+    // 4. Set interval to 15 seconds if 0, otherwise let it be incase we manip
+    intervalMilis = intervalMilis === 0 ? 15000 : intervalMilis;
+
+}, intervalMilis);  
+
+
 // ███████╗███████╗██████╗ ██╗   ██╗███████╗    ██╗████████╗    
 // ██╔════╝██╔════╝██╔══██╗██║   ██║╚══███╔╝    ██║╚══██╔══╝    
 // ███████╗█████╗  ██████╔╝██║   ██║  ███╔╝     ██║   ██║       
 // ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝ ███╔╝      ██║   ██║       
 // ███████║███████╗██║  ██║ ╚████╔╝ ███████╗    ██║   ██║       
-// ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝    ╚═╝   ╚═╝       
-                                                                
-Promise.all(promises).then(() => {
-    console.log(digitalOcean.droplets);
-
-    app.listen(process.env.port, () => utils.log(`Listening on port ${process.env.port}!`));
-}).catch((err) => {
-    utils.log(`Error! ${err}`)  
-});
+// ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝    ╚═╝   ╚═╝     
+app.listen(process.env.port, () => utils.log(`:cumbical_chick: Listening on port ${process.env.port}!`));
+   
